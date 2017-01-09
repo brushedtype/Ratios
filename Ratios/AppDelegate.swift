c9acc8364f97b9a0fc35ac5a4644453765191a5a
Ed Wellbrook <edwellbrook@gmail.com>
2017-01-09 12:55:42 +0000

    Initial commit

---
+++ Ratios/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Ratios
+//
+//  Created by Edward Wellbrook on 09/01/2017.
+//  Copyright © 2017 Brushed Type. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
---
+++ Ratios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ratios
+//
+//  Created by Edward Wellbrook on 09/01/2017.
+//  Copyright © 2017 Brushed Type. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
