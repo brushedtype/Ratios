802055edfec578735eb120b3aca0bde2bb5fe0ef
Ed Wellbrook <edwellbrook@gmail.com>
2017-09-20 21:33:12 +0100

    Document the theme colours

--- Ratios/Theme.swift
+++ Ratios/Theme.swift
@@ -11,11 +11,26 @@ import UIKit
 struct Theme {
     private init() {}
 
+
+    /// The normal keyboard button colour
     static let keyboardButtonColour = UIColor(red:0.98, green:0.96, blue:0.93, alpha:1.0)
+
+    /// The active/highlighted keyboard button colour
     static let keyboardButtonHighlightColour = UIColor(red:0.77, green:0.70, blue:0.63, alpha:1.0)
+
+    /// The application background colour
     static let backgroundColour = UIColor(red: 0.87256, green: 0.79711, blue: 0.71713, alpha: 1.0)
+
+    /// The application tint and text colour
     static let accentColour = UIColor(red:0.31, green:0.28, blue:0.25, alpha:1.0)
-    static let subtleAccentColour = Theme.accentColour.withAlphaComponent(0.8)
+
+    /// A subtle version of the application text colour
+    static let subtleAccentColour  = Theme.accentColour.withAlphaComponent(0.8)
+
+    /// The bordered button colour
     static let buttonHighlightColour = Theme.accentColour.withAlphaComponent(0.4)
+
+    /// The application shadow colour
     static let shadowColour = UIColor.black
+
 }
