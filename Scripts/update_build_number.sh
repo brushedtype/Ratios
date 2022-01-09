6626421300be346381dca29112689b848d8f6e1a
Ed Wellbrook <edwellbrook@gmail.com>
2022-01-09 16:09:49 +0000

    Fix build number script

--- Scripts/update_build_number.sh
+++ Scripts/update_build_number.sh
@@ -5,7 +5,7 @@
 #  Run this script after the 'Copy Bundle Resources' build phase
 #  Ref: http://tgoode.com/2014/06/05/sensible-way-increment-bundle-version-cfbundleversion-xcode/
 
-branch=${1:-'master'}
+branch=${1:-'main'}
 buildNumber=$(expr $(git rev-list $branch --count) - $(git rev-list HEAD..$branch --count))
 echo "Updating build number to $buildNumber using branch '$branch'."
 echo "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
